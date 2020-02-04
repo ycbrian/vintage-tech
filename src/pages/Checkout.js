eb6a5b71df25cbf5ebfaf268042f819bdbc6a9f7
Brian <brian19931009@gmail.com>
2020-02-04 23:44:39 +0800

    Working on strapi, stripe transaction,backend production

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -3,10 +3,16 @@ import { CartContext } from "../context/cart";
 import { UserContext } from "../context/user";
 import { useHistory } from "react-router-dom";
 import EmptyCart from "../components/Cart/EmptyCart";
+import {
+  CardElement,
+  StripeProvider,
+  Elements,
+  injectStripe
+} from "react-stripe-elements";
 // react-stripe-elements
 import submitOrder from "../strapi/submitOrder";
 
-export default function Checkout(props) {
+function Checkout(props) {
   const { cart, total, clearCart } = useContext(CartContext);
   const { user, showAlert, hideAlert, alert } = useContext(UserContext);
   const history = useHistory();
@@ -15,7 +21,38 @@ export default function Checkout(props) {
   const [error, setError] = useState("");
   const isEmpty = !name || alert.show;
   async function handleSubmit(e) {
+    showAlert({ msg: "submitting order... please wait" });
     e.preventDefault();
+    const response = await props.stripe
+      .createToken()
+      .catch(error => console.log(error));
+    console.log(response);
+    const { token } = response;
+    if (token) {
+      setError("");
+      const { id } = token;
+      let order = await submitOrder({
+        name: name,
+        total: total,
+        items: cart,
+        stripeTokenId: id,
+        userToken: user.token
+      });
+      if (order) {
+        showAlert({ msg: "your order is complete" });
+        clearCart();
+        history.push("/");
+        return;
+      } else {
+        showAlert({
+          msg: "there was an error with your order. please try again!",
+          type: "danger"
+        });
+      }
+    } else {
+      hideAlert();
+      setError(response.error.message);
+    }
   }
   if (cart.length < 1) return <EmptyCart />;
   return (
@@ -48,6 +85,7 @@ export default function Checkout(props) {
             enter any 3 digits for the CVC
           </p>
         </div>
+        <CardElement className="card-element"></CardElement>
         {error && <p className="form-empty">{error}</p>}
         {isEmpty ? (
           <p className="form-empty">please fill out name field</p>
@@ -64,3 +102,17 @@ export default function Checkout(props) {
     </section>
   );
 }
+
+const CardForm = injectStripe(Checkout);
+
+const StripeWrapper = () => {
+  return (
+    <StripeProvider apiKey="pk_test_PXhEMpRthtAdJuverp8ORAf300sYsZ2yvq">
+      <Elements>
+        <CardForm></CardForm>
+      </Elements>
+    </StripeProvider>
+  );
+};
+
+export default StripeWrapper;
--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,2 +1,2 @@
 // url
-export default "http://localhost:1337";
+export default "https://brian-vintageapi.herokuapp.com";
